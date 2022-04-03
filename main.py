63b037882789b9141c39f9217c60bed55c6dc48f
UnkwUsr <ktoto2707043@gmail.com>
2022-04-03 23:46:37 +0300

    feat: get emails

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 from time import time
 
+
 def getStudents(group):
     group_page = urlopen("https://uni-dubna.ru/LK/GetGroup?number=" + group)
 
@@ -16,26 +17,45 @@ def getStudents(group):
             url = group_page.readline().decode("utf-8")[77:-50]
             url = "https://uni-dubna.ru" + url
 
-            students.append([group, full_name, url])
+            uid = url[-36:]
+            email = getEmail(uid)
+
+            students.append([group, full_name, email, url])
 
     print("Done group " + group)
 
     return students
+
+
+def getEmail(uid):
+    user_page = urlopen("https://uni-dubna.ru/LK/ProfilePublic?userId=" + uid)
+    for line in user_page:
+        line = line.decode("utf-8")
+
+        if "Email" in line:
+            email = line[41:-7]
+
+            return email
+
+    return None
+
 
 def storeToFile(students, filename):
     fout = open(filename, 'a')
     for student in students:
         group = student[0]
         full_name = student[1]
-        url = student[2]
+        email = student[2]
+        url = student[3]
 
-        fout.write(group + "," + full_name + "," + url + "\n")
+        fout.write(group + "," + full_name + "," + email + "," + url + "\n")
 
     fout.close()
 
+
 filename = "result_students_" + str(int(time())) + ".csv"
 fout = open(filename, 'w')
-fout.write("group,full_name,url\n")
+fout.write("group,full_name,email,url\n")
 fout.close()
 
 groups_file = open("groups", 'r')
@@ -45,4 +65,3 @@ for group in groups_file:
     students = getStudents(group)
     storeToFile(students, filename)
 groups_file.close()
-
