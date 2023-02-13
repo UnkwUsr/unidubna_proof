f22ea7e9d9437468c7143c3da96dbca3624b1cbd
UnkwUsr <ktoto2707043@gmail.com>
2023-02-13 22:03:00 +0300

    feat: get photo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from urllib.request import urlopen
 from time import time
+import os
+import base64
 
 
 def getStudents(group):
@@ -11,6 +13,7 @@ def getStudents(group):
 
         if "<span>" in line:
             full_name = line[38:-9]
+            print("Parsing " + full_name)
 
             # skip one line
             group_page.readline()
@@ -18,47 +21,63 @@ def getStudents(group):
             url = "https://uni-dubna.ru" + url
 
             uid = url[-36:]
-            email = getEmail(uid)
+            (email, photo) = getEmailAndPhoto(uid)
 
-            students.append([group, full_name, email, url])
+            students.append([group, full_name, email, url, photo])
 
     print("Done group " + group)
 
     return students
 
 
-def getEmail(uid):
+def getEmailAndPhoto(uid):
     user_page = urlopen("https://uni-dubna.ru/LK/ProfilePublic?userId=" + uid)
-    for line in user_page:
-        line = line.decode("utf-8")
+    lines = user_page.readlines()
 
-        if "Email" in line:
-            email = line[41:-7]
+    imgline = lines[6]
+    photo = b""
+    if b"img-raised" in imgline:
+        photoBase64 = imgline[79:-4]
+        photo = base64.b64decode(photoBase64)
 
-            return email
+    email = lines[33][41:-7].decode("utf-8")
 
-    return None
+    return (email, photo)
 
 
 def storeToFile(students, filename):
-    fout = open(filename, 'a')
+    fout = open(filename, "a")
     for student in students:
         group = student[0]
         full_name = student[1]
         email = student[2]
         url = student[3]
+        photo = student[4]
 
         fout.write(group + "," + full_name + "," + email + "," + url + "\n")
+
+        uid = url[-36:]
+
+        # use uid in filename because there is some people with 2 profiles
+        fphotoout = open(
+            "photos_" + filename + "/" + group + "_" + full_name + "_" + uid +
+            ".jpeg",
+            "wb",
+        )
+        fphotoout.write(photo)
+        fphotoout.close()
 
     fout.close()
 
 
 filename = "result_students_" + str(int(time())) + ".csv"
-fout = open(filename, 'w')
+fout = open(filename, "w")
 fout.write("group,full_name,email,url\n")
 fout.close()
 
-groups_file = open("groups", 'r')
+os.makedirs("photos_" + filename, exist_ok=True)
+
+groups_file = open("groups", "r")
 for group in groups_file:
     group = group[:-1]
 
